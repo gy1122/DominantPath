d4e91717075696f1a916f7dccef5b19e9095e973
gy1122 <tiger.yang.g@gmail.com>
2017-03-06 15:22:43 -0600

    test for heatmap

--- main.cpp
+++ main.cpp
@@ -30,11 +30,13 @@ int main(int argc, const char * argv[]) {
     int display_paths = 1;
 
     // This program finds the paths from pts[0] to pts[1]
-    Point pts[2];
+    Point pts[3];
     pts[0].x = 2.5;
     pts[0].y = 1.5;
     pts[1].x = 9.3;
     pts[1].y = 12.3;
+    pts[2].x = 7.9;
+    pts[2].y = 11.1;
 
     int limit = 50;
 
@@ -70,17 +72,31 @@ int main(int argc, const char * argv[]) {
     }
 
 
-    DominantPath dmp(&flp, 2, pts);
-    dmp.generateG2();
+    DominantPath dmp(&flp, 3, pts);
+    //dmp.generateG2();
 
     Path *paths = new Path[limit];
-    int npaths;
-    int count = dmp.BreakPoints(0, 1, limit, paths, npaths);
-    printf("%d relaxation performed.\n", count);
+    int npaths = 2;
+    //int count = dmp.BreakPoints(0, 2, limit, paths, npaths);
+    //printf("%d relaxation performed.\n", count);
+    //dmp.Dijkstra_all_dest(100, paths);
+    
+    /*
+    dmp.Approx_all_dest(5.0, 0.5, paths);
     if (display_paths) {
         dmp.printPaths(npaths, paths);
     }
-
+    
+    dmp.BreakPoints(0, 2, limit, paths, npaths);
+    dmp.printPaths(npaths, paths);
+    
+    for (int i = 0; i < npaths; i++) {
+        printf("%f\n", (paths[i].L+5.0*std::log(paths[i].D)));
+    }
+     */
+    
+    dmp.heatmap(5.0, 0.5, 45, 45, 15, 15, 5);
+    
     delete [] paths;
 
     return 0;
